d7ca68c42e613b597e1c1868e04844c5026c79bd
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-10-28 12:35:34 +0300

    Update Counter.styled.jsx

--- src/components/Counter/Counter.styled.jsx
+++ src/components/Counter/Counter.styled.jsx
@@ -2,11 +2,12 @@ import styled from '@emotion/styled';
 
 export const CounterList = styled.ul`
   background-color: #fff;
-  border: 1px dashed #557b83;
+  border: 1px solid #557b83;
   padding: 20px;
   border-radius: 10px;
   width:500px;
   margin-left:20px;
+   box-shadow: 0px 5px 14px 4px rgba(94, 57, 18, 0.82);
 `;
 
 export const CounterItem = styled.li`
